9875a7775ab48782d460ffb3f181256e5fc89bb3
Kseniia <ksenjadzjubina@gmail.com>
2022-07-21 14:36:37 +0300

    Задача 29 с вводом массива

---
+++ Task2901/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите массив из восьми чисел: ");
+{
+int [] myArray = new int[8];
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}:   ");
+    myArray[i] = Convert.ToInt32(Console.ReadLine()); 
+}
+
+
+Console.WriteLine("\nВывод массива:");
+    for ( int i = 0; i < myArray.Length; i++)
+    {
+    Console.Write(myArray[i] + " ");
+    }
+     Console.ReadLine();
+}
+
