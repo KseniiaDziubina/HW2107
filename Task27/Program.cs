0556ced3c78bbb374bf66d2082128eab6d16e877
Kseniia <ksenjadzjubina@gmail.com>
2022-07-21 14:21:32 +0300

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число    "); 
+int Number = Convert.ToInt32(Console.ReadLine()); 
+
+int Sum = 0;
+while (Number > 0)
+{
+    Sum += Number %10;
+    Number = Number/10;
+}
+Console.WriteLine($"Сумма цифр в числе равна: {Sum}");
