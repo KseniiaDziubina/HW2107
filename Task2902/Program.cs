1c42659ddeebbacb638cf60e925738f64c6ac48b
Kseniia <ksenjadzjubina@gmail.com>
2022-07-21 14:42:43 +0300

    Задача 29 со случайным массивом

---
+++ Task2902/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Массив:   ");
+int [] Array2 = new int[8];
+Random range = new Random();
+
+    for (int i = 0; i < Array2.Length; i++)
+         {
+            Array2[i] = range.Next(99);
+            Console.WriteLine(Array2[i]);
+         }
+
